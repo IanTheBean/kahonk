b568dec04dc7e2ae907cd785b79830ae86a5ba54
IanTheBean <68975565+IanTheBean@users.noreply.github.com>
2021-09-27 11:05:24 -0500

    Create room.js

---
+++ server/room.js
@@ -0,0 +1,21 @@
+export const room = {
+    questions:[
+        { 
+            question: "What is 1 + 4?",
+            incorrect: ["4", "1", "7"],
+            correct: "5"
+        },
+        { 
+            question: "What is 5 + 4?",
+            incorrect: ["4", "1", "7"],
+            correct: "9"
+        }
+    ],
+    questionNumber: 0,
+    players: [
+         {name: "Coder1000", score: 1625, socket:"id81636572h2jg62ig46o436"}, 
+         {name: "Person", score: 4671, socket:"id81636572h2jg62ig46o436"}
+        ],
+    code: 123456
+
+}
